b678b7da7ddfcd212fc44b23c7a1eef654103f86
Somar Kesen <kesen.somar.99@gmail.com>
2024-04-22 17:19:46 +0300

    Upload the Package Files

---
+++ src/useOSRedirect.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+
+const useOSRedirect = (androidUrl: string, iosUrl: string, defaultUrl: string) => {
+  useEffect(() => {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Redirect based on the user agent
+    if (/android/i.test(userAgent)) {
+      window.location.href = androidUrl;
+    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      window.location.href = iosUrl;
+    } else {
+      window.location.href = defaultUrl;
+    }
+  }, [androidUrl, iosUrl, defaultUrl]); // Dependencies array to avoid unnecessary redirects
+};
+
+export default useOSRedirect;
